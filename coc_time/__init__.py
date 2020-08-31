221e887234d558f0b1605029a939ae86f230dfbb
sralloza <sralloza@gmail.com>
2020-08-31 16:35:08 +0200

    style: lint init

--- coc_time/__init__.py
+++ coc_time/__init__.py
@@ -4,6 +4,7 @@ import sys
 from coc_time.crontab import CrontabManager
 from coc_time.ssh import Machines
 
+
 def parse_args():
     parser = ArgumentParser(prog="Clash of Clans")
     parser.add_argument(
@@ -11,16 +12,16 @@ def parse_args():
         nargs="?",
         default=Machines.volvo,
         type=Machines.validate,
-        help="machine to use"
+        help="machine to use",
     )
-    parser.add_argument("--add-cron", "-a", help="Add cron line",  action="store_true")
+    parser.add_argument("--add-cron", "-a", help="Add cron line", action="store_true")
     parser.add_argument("--write", "-w", help="Update cron", action="store_true")
 
     return parser.parse_args()
 
 
 def main():
-    options =  vars(parse_args())
+    options = vars(parse_args())
     Machines.set_current(options["machine"])
 
     print("Using machine %r" % Machines.get_current().name)
