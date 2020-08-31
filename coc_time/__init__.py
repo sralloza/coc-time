4531cab3fd1d32796142568b1c176ab55c900494
sralloza <sralloza@gmail.com>
2020-08-31 13:23:13 +0200

    feat: move main function to coc_time/__init__.py

---
+++ coc_time/__init__.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+import sys
+
+from coc_time.crontab import CrontabManager
+from coc_time.ssh import Machines
+
+def parse_args():
+    parser = ArgumentParser(prog="Clash of Clans")
+    parser.add_argument(
+        "machine",
+        nargs="?",
+        default=Machines.volvo,
+        type=Machines.validate,
+        help="machine to use"
+    )
+    parser.add_argument("--add-cron", "-a", help="Add cron line",  action="store_true")
+    parser.add_argument("--read-only", help="Do not save cron", action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    options =  vars(parse_args())
+    Machines.set_current(options["machine"])
+
+    print("Using machine %r" % Machines.get_current().name)
+    cron_mng = CrontabManager.get_current_crons()
+
+    print(cron_mng)
+
+    if options["add_cron"]:
+        cron_mng.add_cron()
+
+    if not options["read_only"] or options["add_cron"]:
+        result = cron_mng.save_to_server()
+        print(f"[{result}]")
--- run.py
+++ run.py
@@ -1,23 +1,4 @@
-import sys
-
-from coc_time.crontab import CrontabManager
-from coc_time.ssh import Machines
-
-
-def main():
-    if sys.argv[1:]:
-        Machines.set_current(sys.argv[1])
-    else:
-        Machines.set_current(Machines.volvo)
-
-    print("Using machine %r" % Machines.get_current().name)
-    cron_mng = CrontabManager.get_current_crons()
-    print(cron_mng)
-
-    cron_mng.add_cron()
-
-    result = cron_mng.save_to_server()
-    print(f"[{result}]")
+from coc_time import main
 
 
 if __name__ == "__main__":
